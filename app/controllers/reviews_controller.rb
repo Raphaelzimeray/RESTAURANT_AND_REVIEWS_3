c950907a8fa238a69e582cc2167cff9f91b37fc6
Raphael Zimeray <raphaelzimeray@me.com>
2022-12-15 16:23:13 +0100

    model done

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,24 @@
+class ReviewsController < ApplicationController
+
+  def new
+    @review = Review.new
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def create
+    @review = Review.create(params_review)
+    @review.restaurant = Restaurant.find(params[:restaurant_id])
+    @review.user = current_user
+    if @review.save!
+      redirect_to restaurant_path(@review.restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def params_review
+    params.require(:review).permit(:rating, :comment)
+  end
+end
